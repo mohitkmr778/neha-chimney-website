2b46123ce0d984bd46bc2c05fbeaa8d055af79f7
mohitkmr778 <abhayachimneyservices@gmail.com>
2025-10-24 19:30:04 +0530

    Update Footer.jsx

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -146,7 +146,7 @@ export default function Footer() {
                           
                             <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d437.98678877107756!2d77.43824429429141!3d28.572936264724145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjjCsDM0JzIyLjYiTiA3N8KwMjYnMTguNyJF!5e0!3m2!1sen!2sin!4v1760426702915!5m2!1sen!2sin"
                                 width="100%" height="180"
-                                style="border: 0" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title="Google Map"></iframe>
+                                style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title="Google Map"></iframe>
                         </div>
                     </div>
                 </div>
