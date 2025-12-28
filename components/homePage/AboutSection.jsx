b5ebee018707d63e63433e6842d4dfccda91a728
mohitkmr778 <abhayachimneyservices@gmail.com>
2025-12-28 14:12:20 +0530

    Update AboutSection.jsx

--- components/homePage/AboutSection.jsx
+++ components/homePage/AboutSection.jsx
@@ -5,10 +5,10 @@ export default function AboutSection() {
         <section className="bg-[#18344a] py-10">
             <div className="max-w-[900px] mx-auto px-4 text-center">
                 <h2 className="text-3xl font-bold mb-4 text-[#ffb300] drop-shadow">
-                    About  Neha Chimney & Repair Services 
+                    About  Neha Chimney & RO Repair Services 
                 </h2>
                 <p className="text-lg text-[#f5f7fa] mb-2">
-                  We at Neha Chimney & Repair Services are proud to be among the most reputable companies for kitchen appliance maintenance.
+                  We at Neha Chimney & RO Repair Services are proud to be among the most reputable companies for kitchen appliance maintenance.
                     With years of expertise, we provide quick, dependable, and reasonably priced solutions for every home. We specialize in 
                     chimney repair, installation,and maintenance services. Our goal is to keep your kitchen always smoke-free, 
                     spotless, and completely operational.
@@ -17,7 +17,7 @@ We are aware of how vital a healthy chimney is to your house. To achieve long-la
 our team of knowledgeable and trained experts uses cutting-edge equipment and authentic replacement components. We effectively 
             identify and repair any chimney issues, whether they are caused by a clogged filter, a motor problem, an odd noise,
                     or inadequate suction.
-                    Our first goal at Neha Chimney & Repair Services is client happiness. We are committed to providing our customers with the bestvalue
+                    Our first goal at Neha Chimney & RO Repair Services is client happiness. We are committed to providing our customers with the bestvalue
                     for their money by upholding clear pricing, timely responses, and professional service. Your one-stop shop for all kitchen 
                     appliance needs, we provide chimney services as well as installation and repair for gas stoves and hobs.
 
